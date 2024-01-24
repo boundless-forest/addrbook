162cd44e1b4d84d5b0222a6109bd239832b9d36a
bear <boundless.forest@outlook.com>
2024-01-24 11:15:57 +0800

    Dev the cli part

---
+++ cli.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type WorkSpaceCommand struct {
+	fs *flag.FlagSet
+}
+
+func NewWorkSpaceCommand() *WorkSpaceCommand {
+	wsc := WorkSpaceCommand{
+		fs: flag.NewFlagSet("workspace", flag.ContinueOnError),
+	}
+	return &wsc
+}
+func (wsc *WorkSpaceCommand) Init(args []string) error {
+	return wsc.fs.Parse(args)
+}
+func (wsc *WorkSpaceCommand) Name() string {
+	return wsc.fs.Name()
+}
+func (wsc *WorkSpaceCommand) Run() error {
+	if len(os.Args) <= 2 {
+		printUsage()
+		return errors.New("workspace command requires subcommand")
+	}
+
+	subCmds := []Runner{
+		NewWsNewCommand(),
+		NewWsListCommand(),
+		NewWsSaveCommand(),
+		NewWsUpdateCommand(),
+		NewWsDeleteCommand(),
+	}
+
+	for _, cmd := range subCmds {
+		if cmd.Name() == os.Args[2] {
+			cmd.Init(os.Args[3:])
+			return cmd.Run()
+		}
+	}
+
+	return fmt.Errorf("unknown command %s", os.Args[2])
+}
+
+// workspace new ...
+
+type WsNewCommand struct {
+	fs *flag.FlagSet
+
+	name string
+}
+
+func NewWsNewCommand() *WsNewCommand {
+	new := WsNewCommand{
+		fs: flag.NewFlagSet("new", flag.ContinueOnError),
+	}
+	new.fs.StringVar(&new.name, "name", "", "The name of the workspace")
+	return &new
+}
+func (new *WsNewCommand) Name() string {
+	return new.fs.Name()
+}
+func (new *WsNewCommand) Init(args []string) error {
+	return new.fs.Parse(args)
+}
+func (new *WsNewCommand) Run() error {
+	fmt.Println("This is the workspace command... new subcommand, name: ", new.name)
+	if new.name == "" {
+		printUsage()
+		return errors.New("workspace name is required")
+	}
+
+	return nil
+}
+
+// workspace del ...
+
+type WsDelCommand struct {
+	fs *flag.FlagSet
+
+	name string
+}
+
+func NewWsDelCommand() *WsDelCommand {
+	del := WsDelCommand{
+		fs: flag.NewFlagSet("del", flag.ContinueOnError),
+	}
+	del.fs.StringVar(&del.name, "name", "", "The name of the workspace")
+	return &del
+}
+func (del *WsDelCommand) Name() string {
+	return del.fs.Name()
+}
+func (del *WsDelCommand) Init(args []string) error {
+	return del.fs.Parse(args)
+}
+func (del *WsDelCommand) Run() error {
+	fmt.Println("This is the workspace command... del subcommand, name: ", del.name)
+	if del.name == "" {
+		printUsage()
+		return errors.New("workspace name is required")
+	}
+
+	return nil
+}
+
+// workspace list ...
+
+type WsListCommand struct {
+	fs *flag.FlagSet
+}
+
+func NewWsListCommand() *WsListCommand {
+	list := WsListCommand{
+		fs: flag.NewFlagSet("list", flag.ContinueOnError),
+	}
+	return &list
+}
+func (list *WsListCommand) Name() string {
+	return list.fs.Name()
+}
+func (list *WsListCommand) Init(args []string) error {
+	return list.fs.Parse(args)
+}
+func (list *WsListCommand) Run() error {
+	fmt.Println("list subcommand TODO")
+	return nil
+}
+
+// workspace save ...
+
+type WsSaveCommand struct {
+	fs *flag.FlagSet
+
+	workspace string
+	contract  string
+	address   string
+}
+
+func NewWsSaveCommand() *WsSaveCommand {
+	save := WsSaveCommand{
+		fs: flag.NewFlagSet("save", flag.ContinueOnError),
+	}
+	save.fs.StringVar(&save.workspace, "workspace", "", "The name of the workspace")
+	save.fs.StringVar(&save.contract, "contract", "", "The name of the contract")
+	save.fs.StringVar(&save.address, "address", "", "The address of the contract")
+	return &save
+}
+func (save *WsSaveCommand) Name() string {
+	return save.fs.Name()
+}
+func (save *WsSaveCommand) Init(args []string) error {
+	return save.fs.Parse(args)
+}
+func (save *WsSaveCommand) Run() error {
+	fmt.Println("save subcommand TODO, workspace: ", save.workspace, " contract: ", save.contract, " address: ", save.address)
+	return nil
+}
+
+// workspace update ...
+
+type WsUpdateCommand struct {
+	fs *flag.FlagSet
+
+	workspace string
+	contract  string
+	address   string
+}
+
+func NewWsUpdateCommand() *WsSaveCommand {
+	update := WsSaveCommand{
+		fs: flag.NewFlagSet("update", flag.ContinueOnError),
+	}
+	update.fs.StringVar(&update.workspace, "workspace", "", "The name of the workspace")
+	update.fs.StringVar(&update.contract, "contract", "", "The name of the contract")
+	update.fs.StringVar(&update.address, "address", "", "The address of the contract")
+	return &update
+}
+func (update *WsUpdateCommand) Name() string {
+	return update.fs.Name()
+}
+func (update *WsUpdateCommand) Init(args []string) error {
+	return update.fs.Parse(args)
+}
+func (update *WsUpdateCommand) Run() error {
+	fmt.Println("update subcommand TODO, workspace: ", update.workspace, " contract: ", update.contract, " address: ", update.address)
+	return nil
+}
+
+// workspace delete ...
+
+type WsDeleteCommand struct {
+	fs *flag.FlagSet
+
+	workspace string
+	contract  string
+	address   string
+}
+
+func NewWsDeleteCommand() *WsSaveCommand {
+	delete := WsSaveCommand{
+		fs: flag.NewFlagSet("delete", flag.ContinueOnError),
+	}
+	delete.fs.StringVar(&delete.workspace, "workspace", "", "The name of the workspace")
+	delete.fs.StringVar(&delete.contract, "contract", "", "The name of the contract")
+	delete.fs.StringVar(&delete.address, "address", "", "The address of the contract")
+	return &delete
+}
+func (delete *WsDeleteCommand) Name() string {
+	return delete.fs.Name()
+}
+func (delete *WsDeleteCommand) Init(args []string) error {
+	return delete.fs.Parse(args)
+}
+func (delete *WsDeleteCommand) Run() error {
+	fmt.Println("delete subcommand TODO, workspace: ", delete.workspace, " contract: ", delete.contract, " address: ", delete.address)
+	return nil
+}
+
+type Runner interface {
+	Init([]string) error
+	Run() error
+	Name() string
+}
+
+func printUsage() {
+	fmt.Println("addrbook Usage:")
+	fmt.Println("  workspace new --name <name>,                                                     Create a new workspace to manage the contract addresses.")
+	fmt.Println("  workspace del --name <name>,                                                     Delete a new workspace to manage the contract addresses.")
+	fmt.Println("  workspace list,                                                                  List the workspaces managed by the current user.")
+	fmt.Println("  workspace save   --workspace <workspace> --contract <contract> --address <addr>, Save the contract address into the workspace.")
+	fmt.Println("  workspace update --workspace <workspace> --contract <contract> --address <addr>, Update the contract address into the workspace.")
+	fmt.Println("  workspace delete --workspace <workspace> --contract <contract> --address <addr>, Update the contract address into the workspace.")
+}
+
+func run(args []string) error {
+	cmds := []Runner{
+		NewWorkSpaceCommand(),
+	}
+
+	for _, cmd := range cmds {
+		if cmd.Name() == args[0] {
+			cmd.Init(os.Args[2:])
+			return cmd.Run()
+		}
+	}
+
+	printUsage()
+	return fmt.Errorf("unknown command %s", os.Args[1])
+}
